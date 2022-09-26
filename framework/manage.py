5eee8663447f44cc6878b6347b0984f9f2979648
ioqt <ioqt00@gresille.org>
2022-09-26 03:14:02 -0400

    moved things

--- framework/manage.py
+++ framework/manage.py
@@ -25,7 +25,10 @@ def list_(ctx: Context, args_list: list):
     challenge = args.challenge
 
     if not challenge and not ctf:
-        challenge = True
+        if ctx.selected_ctf is None:
+            ctf=True
+        else:
+            challenge=True
     if challenge and ctx.selected_ctf is None:
         ctx.send("Bad arguments: please select a ctf first")
         sys.exit()
@@ -55,23 +58,27 @@ def list_ctf(ctx: Context):
 def select(ctx: Context, args_list: list):
     logging.debug("Selecting ctf or challenge")
     parser = argparse.ArgumentParser(description='')
-    parser.add_argument("--ctf-id", help="List ctf")
-    parser.add_argument("--challenge-id", help="List challenges in selected ctf")
+    parser.add_argument('id', type=int, help='Id of either chall or ctf to select')
+    parser.add_argument("--ctf-id", action='store_true', help="Select ctf")
+    parser.add_argument("--challenge-id", action='store_true', help="Select challenge in selected ctf")
     args = parser.parse_args(args_list)
 
     ctf_id = args.ctf_id
     challenge_id = args.challenge_id
+    id = args.id
 
-    if challenge_id is None and ctf_id is None:
-        ctx.send("Bad arguments: please give either a ctf or challenge id")
-        sys.exit()
-    if challenge_id is not None and ctx.selected_ctf is None:
+    if not challenge_id and not ctf_id:
+        if ctx.selected_ctf is None:
+            ctf_id = True
+        else:
+            challenge_id = True
+    if challenge_id and ctx.selected_ctf is None:
         ctx.send("Bad arguments: please select a ctf first")
         sys.exit()
-    if challenge_id is not None:
-        select_challenge(ctx, int(challenge_id))
-    elif ctf_id is not None:
-        select_ctf(ctx, int(ctf_id))
+    if challenge_id:
+        select_challenge(ctx, id)
+    elif ctf_id:
+        select_ctf(ctx, id)
 
 def select_challenge(ctx: Context, challenge_id: int) -> None:
     logging.debug("Selecting challenge")
@@ -91,8 +98,12 @@ def select_ctf(ctx: Context, ctf_id: int):
     ctx.send("CTF id not found in CTF list")
     sys.exit()
 
-def flag(ctx: Context, flag: Optional[str]= None):
-    not_implemented()
+def flag(ctx: Context, args_list: list):
+    logging.debug("Flagging challenge")
+    if ctx.selected_challenge is None:
+        ctx.send("You must select a challenge first")
+        sys.exit()
+    ctx.selected_challenge.solved = True
 
 def create_ctf(ctx: Context, args: list):
     logging.info("Launching appropriate create_ctf")
@@ -101,6 +112,9 @@ def create_ctf(ctx: Context, args: list):
 
 def show(ctx: Context, args_list: list):
     logging.debug("Showing challenge")
+    if ctx.selected_challenge is None:
+        ctx.send("You must select a challenge first")
+        sys.exit()
     ticket_path = ctx.selected_challenge.directory.joinpath("ticket")
     msg = ticket_path.read_text(encoding="utf-8")
     ctx.send(msg)
